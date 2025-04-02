a782847c1bbaa95b06d951fbeec383ebd9a667fe
Fidan Cindoğlu <123310124+FidanCindoglu@users.noreply.github.com>
2025-04-02 13:53:56 +0300

    Add files via upload

---
+++ UNITY_PROJE_DOSYALARI_ZarOyunu/ZarOyunu/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZarOyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TavlaZari tavlaZari1 = new TavlaZari(8);
+            Console.WriteLine("Zar 1: " + tavlaZari1.ZarAt());
+
+            //tavlaZari2 adında yeni bir obje oluşturun
+            //Bu obje ile zar atıp konsola yazdırıp 1. ile yarıştırın
+        }
+    }
+}
