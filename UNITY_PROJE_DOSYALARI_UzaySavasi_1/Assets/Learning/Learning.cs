011c6aaa4707902e5833009e8df8a38cb722eb92
Fidan Cindoğlu <123310124+FidanCindoglu@users.noreply.github.com>
2025-04-02 14:33:46 +0300

    Add files via upload

---
+++ UNITY_PROJE_DOSYALARI_UzaySavasi_1/Assets/Learning/Learning.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Learning : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int yokEdilenAsteroid = 5;
+        int oyuncuHp = 50;
+
+        Debug.Log(yokEdilenAsteroid <= 5 && oyuncuHp > 50);
+
+        if(yokEdilenAsteroid >= 10 || oyuncuHp >= 80)
+        {
+            //Debug.Log("Çok güzel");
+        }
+
+        //if (yokEdilenAsteroid == 20)
+        //{
+        //    Debug.Log("Tebrikler oyunu kazandınız!");
+        //} else if(yokEdilenAsteroid == 25)
+        //{
+        //    Debug.Log("Tebrikler birinci oldunuz");
+        //} else
+        //{
+        //    Debug.Log("Maalesef oyunu kaybettiniz :(");
+        //}
+
+        //switch(yokEdilenAsteroid)
+        //{
+        //    case 1:
+        //        Debug.Log("Güzel başlangıç");
+        //        break;
+        //    case 10:
+        //        Debug.Log("Bu işte çok iyisin");
+        //        break;
+        //    default:
+        //        Debug.Log("Maalesef oyunu kaybettiniz");
+        //        break;
+        //}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
